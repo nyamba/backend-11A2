a7f3f47b3a560417ec9794aa2675c1ce1f5a1f25
nyambayar <nyambayar@steppelink.mn>
2021-04-27 12:53:52 +0800

    tui editor for create page

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -9,6 +9,7 @@ from flaskr.db import get_db
 bp = Blueprint('blog', __name__)
 
 
+
 @bp.route('/')
 def index():
     db = get_db()
--- flaskr/static/js/create.js
+++ flaskr/static/js/create.js
@@ -2,19 +2,16 @@ $(function (){
 
 	const Editor = toastui.Editor;
 
-	console.log('initial value', $('#body').val());
-
 	const editor = new Editor({
 	  el: document.querySelector('#editor'),
 	  height: '500px',
 	  initialEditType: 'wysiwyg',
-	  initialValue: $('#body').val()
 	});
+
+	console.log('submit button', $('.submit-button'))
 
 	$('.submit-button').on('click', function(evt){
 		evt.preventDefault();
-		//console.log(editor)
-		//alert('submitting' + editor.getMarkdown());
 
 		$('#body').val(editor.getMarkdown());
 		$('#post-form').submit();
